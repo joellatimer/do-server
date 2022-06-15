4cbfebef5ad7bc08bf48986edf3e28129f0d9bf3
Joel Latimer <joellatimer@outlook.com>
2022-06-15 14:39:00 -0400

    Back to cors

--- app.js
+++ app.js
@@ -15,13 +15,7 @@ const res = require('express/lib/response')
 
     
         
-app.use(function(req, res, next) {
-    res.header("Access-Control-Allow-Origin", "*");
-    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
-    res.header("Access-Control-Allow-Headers", "Content-Type, Authorization")
-    res.header("Access-Control-Allow-Methods", "GET, POST, OPTIONS, PUT, DELETE")
-    next();
-})
+app.use(cors())
 app.use(morgan('dev'))
 
 app.use(express.urlencoded({extended:true}))
