760e7b293acce2898fbb05ae42e76ed6211101f0
Joel Latimer <joellatimer@outlook.com>
2022-06-15 16:51:15 -0400

    cors

--- app.js
+++ app.js
@@ -19,7 +19,7 @@ app.use(function(req, res, next) {
     res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
     res.header("Access-Control-Allow-Headers", "Content-Type, Authorization")
     res.header("Access-Control-Allow-Methods", "GET, POST, OPTIONS, PUT, DELETE")
-    res.header("Access-Control-Allow-Private-Network: true")
+    // res.header("Access-Control-Allow-Private-Network: true")
     next();
 } )      
 
