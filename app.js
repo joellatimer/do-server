0064c8d24dd0dd4e0655fbf7b22216b9fe47ed19
Joel Latimer <joellatimer@outlook.com>
2022-06-20 10:06:23 -0400

    Header change

--- app.js
+++ app.js
@@ -15,7 +15,7 @@ const res = require('express/lib/response')
 
     
 app.use(function(req, res, next) {
-    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Origin", "http://204.48.21.44");
     res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
     res.header("Access-Control-Allow-Headers", "Content-Type, Authorization")
     res.header("Access-Control-Allow-Methods", "GET, POST, OPTIONS, PUT, DELETE")
