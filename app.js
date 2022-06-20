921020d20bff9a3c51eb86998dc73a93f0878b14
Joel Latimer <joellatimer@outlook.com>
2022-06-20 10:27:47 -0400

    New Header

--- app.js
+++ app.js
@@ -18,6 +18,7 @@ app.use(function(req, res, next) {
     res.header("Access-Control-Allow-Origin", "http://204.48.21.44");
     res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
     res.header("Access-Control-Allow-Headers", "Content-Type, Authorization")
+    res.header("Access-Control-Content-Type", "text/plain")
     res.header("Access-Control-Allow-Methods", "GET, POST, OPTIONS, PUT, DELETE")
     
     next();
