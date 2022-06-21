7f87761853be015d131d4558f1bf2989bfff7f39
Joel Latimer <joellatimer@outlook.com>
2022-06-20 22:34:54 -0400

    cors

--- app.js
+++ app.js
@@ -14,6 +14,12 @@ const res = require('express/lib/response')
 
 
     
+
+app.use(morgan('dev'))
+
+app.use(express.urlencoded({extended:true}))
+app.use(express.json())
+
 app.use(function(req, res, next) {
     res.header("Access-Control-Allow-Origin", "*");
     res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Authorization" );
@@ -25,12 +31,6 @@ app.use(function(req, res, next) {
     
     next();
 } )      
-
-app.use(morgan('dev'))
-
-app.use(express.urlencoded({extended:true}))
-app.use(express.json())
-
 
 
 app.use('/groups', groupRoutes)
